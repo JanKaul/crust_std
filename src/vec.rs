772a6f9259dcf3bb0393e1b8917de8752898065c
Jan Kaul <jankaul@mailbox.org>
2023-04-22 18:19:04 +0200

    impl nonnull

--- src/vec.rs
+++ src/vec.rs
@@ -2,7 +2,58 @@ use std::alloc::{self, Layout};
 use std::marker::PhantomData;
 use std::mem;
 use std::ops::{Deref, DerefMut};
-use std::ptr::{self, NonNull};
+use std::ptr;
+
+#[repr(C)]
+pub struct NonNull<T: ?Sized> {
+    pointer: *const T,
+}
+
+impl<T> NonNull<T> {
+    #[inline]
+    const unsafe fn new_unchecked(ptr: *mut T) -> Self {
+        // SAFETY: the caller must guarantee that `ptr` is non-null.
+        // unsafe {
+        // assert_unsafe_precondition!("NonNull::new_unchecked requires that the pointer is non-null", [T: ?Sized](ptr: *mut T) => !ptr.is_null());
+        NonNull { pointer: ptr as _ }
+        // }
+    }
+
+    #[inline]
+    pub fn new(ptr: *mut T) -> Option<Self> {
+        if !ptr.is_null() {
+            // SAFETY: The pointer is already checked and is not null
+            Some(unsafe { Self::new_unchecked(ptr) })
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    pub const fn dangling() -> Self {
+        // SAFETY: mem::align_of() returns a non-zero usize which is then casted
+        // to a *mut T. Therefore, `ptr` is not null and the conditions for
+        // calling new_unchecked() are respected.
+        unsafe {
+            let ptr = sptr::invalid_mut::<T>(mem::align_of::<T>());
+            NonNull::new_unchecked(ptr)
+        }
+    }
+
+    #[inline(always)]
+    pub const fn as_ptr(self) -> *mut T {
+        self.pointer as *mut T
+    }
+}
+
+impl<T: ?Sized> Clone for NonNull<T> {
+    #[inline(always)]
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<T: ?Sized> Copy for NonNull<T> {}
 
 #[repr(C)]
 struct RawVec<T> {
