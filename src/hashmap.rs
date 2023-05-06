6ceead91c9a708a9450eec7767c5935e08bccdce
Jan Kaul <jankaul@mailbox.org>
2023-05-06 11:40:10 +0200

    cycle on hashmap insert

--- src/hashmap.rs
+++ src/hashmap.rs
@@ -1,3 +1,4 @@
+use anyhow::anyhow;
 use std::hash::{Hash, Hasher};
 use std::mem;
 use std::{collections::hash_map::DefaultHasher, slice::Iter};
@@ -41,27 +42,30 @@ impl<K: Clone + Hash + Eq, V: Clone> HashMap<K, V> {
         }
     }
 
-    pub fn iter(&self) -> Iter<Option<KeyValue<K, V>>> {
-        self.data.iter()
-    }
-
-    pub fn insert(&mut self, key: &K, value: V) {
+    pub fn insert(&mut self, key: &K, value: V) -> Result<(), anyhow::Error> {
         if self.n_items > 3 * self.capacity / 4 {
             self.double_capacity()
         }
         let index = hash(key) % self.capacity as u64;
-        let temp = self
+        let temp = if let Some(temp) = self
             .data
             .iter_mut()
             .skip(index as usize)
-            .find(|x| x.is_none());
-        if let Some(entry) = temp {
-            *entry = Option::Some(KeyValue {
-                key: key.clone(),
-                value,
-            });
-            self.n_items += 1;
-        }
+            .find(|x| x.is_none())
+        {
+            temp
+        } else {
+            self.data
+                .iter_mut()
+                .find(|x| x.is_none())
+                .ok_or_else(|| anyhow!("Failed to find empty slot in hashmap."))?
+        };
+        *temp = Option::Some(KeyValue {
+            key: key.clone(),
+            value,
+        });
+        self.n_items += 1;
+        Ok(())
     }
 
     pub fn get(&mut self, key: &K) -> std::option::Option<&V> {
@@ -80,6 +84,10 @@ impl<K: Clone + Hash + Eq, V: Clone> HashMap<K, V> {
             .and_then(|x| x.as_ref().map(|x| &x.value))
     }
 
+    pub fn iter(&self) -> Iter<Option<KeyValue<K, V>>> {
+        self.data.iter()
+    }
+
     fn double_capacity(&mut self) {
         self.capacity *= 2;
         let new_data = OwnedSlice::from(vec![Option::None; self.capacity]);
@@ -88,6 +96,7 @@ impl<K: Clone + Hash + Eq, V: Clone> HashMap<K, V> {
         for entry in old_data.into_iter().filter(|x| x.is_some()) {
             if let Option::Some(keyvalue) = entry {
                 self.insert(&keyvalue.key, keyvalue.value)
+                    .expect("Capacity should be enough.")
             }
         }
     }
@@ -103,11 +112,13 @@ mod tests {
     fn hashmap_insert_get() {
         let mut map = HashMap::new();
         let key1: OwnedStr = "test1".to_string().into();
-        map.insert(&key1, 8);
+        map.insert(&key1, 8)
+            .expect("Failed to insert value into hashmap");
         let val1 = map.get(&key1).expect("Failed to get value.");
         assert_eq!(*val1, 8);
         let key2: OwnedStr = "test2".to_string().into();
-        map.insert(&key2, 16);
+        map.insert(&key2, 16)
+            .expect("Failed to insert value into hashmap");
         let val2 = map.get(&key2).expect("Failed to get value.");
         assert_eq!(*val2, 16);
     }
